d43f1742a44d7436cb7d3e86ba0cc157a3cd53ab
Tucker von Holten <icecream@vonholten.name>
2018-06-23 11:51:46 +0200

    Added a controller activity that controls individual activities

---
+++ app/src/main/java/com/tuckervh/messaging/mesa/Controller/QuestionControllerActivity.java
@@ -0,0 +1,15 @@
+package com.tuckervh.messaging.mesa.Controller;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+import com.tuckervh.messaging.mesa.R;
+
+public class QuestionControllerActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_question_controller);
+    }
+}
